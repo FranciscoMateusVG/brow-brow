00ef7fd07fee4a7d2f24a34b94bc328bcbfc1e21
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2023-02-07 23:05:56 -0300

    lp finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import { type AppType } from "next/app";
 import { api } from "../utils/api";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import "../styles/globals.css";
-import Layout from "../containers/layout/Layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </UserProvider>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,52 +1,24 @@
-const keyframes = {
-  wiggle: {
-    "0%, 100%": {
-      transform: "translateX(0)",
-      "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
-    },
-    "50%": {
-      transform: "translateX(15%)",
-      "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
-    },
-  },
-  closeDrawer: {
-    "100%": {
-      transform: "translateX(-225px)",
-      "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
-    },
-  },
-  openDrawer: {
-    "0%": {
-      transform: "translateX(-225px)",
-      "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
-    },
-    "100%": {
-      transform: "translateX(0)",
-      "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
-    },
-  },
-};
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        prim: {
-          light: "#444242",
-          DEFAULT: "#363636",
-          dark: "#262424",
+        primary_text: "#FDFDFD",
+        secundary_text: "#DADADA",
+        black: "#060606",
+        primary: {
+          100: "#0DB760",
+          200: "#088846",
+          300: "#066032",
         },
-        sec: "#CDE8EA",
-        terc: "#F8FAC7",
+        secundary: {
+          100: "#09FF80",
+          200: "#18F684",
+          300: "#16E37A",
+          400: "#13D06F",
+        },
       },
-      animation: {
-        wiggle: "wiggle 1s infinite;",
-        closeDrawer: "closeDrawer 1s ease-in-out forwards",
-        openDrawer: "openDrawer 1s ease-in-out forwards",
-      },
-      keyframes,
     },
   },
   plugins: [],
